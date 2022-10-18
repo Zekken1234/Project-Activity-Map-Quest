d60cd0d575484340c8772109045e081667bb8e99
Zekken1234 <95679675+Zekken1234@users.noreply.github.com>
2022-10-18 16:10:29 +0800

    Update mapquest_parse-json_7.py
    • Unit of measurement for distance

--- mapquest_parse-json_7.py
+++ mapquest_parse-json_7.py
@@ -12,37 +12,76 @@ while True:
     dest = input("Destination: ")
     if dest == "quit" or dest == "q":
         break
-    url = main_api + urllib.parse.urlencode({"key":key, "from":orig, "to":dest})
-    json_data = requests.get(url).json()
-    print("URL: " + (url))
-    json_data = requests.get(url).json()
-    json_status = json_data["info"]["statuscode"]
-    if json_status == 0:
-        print("API Status: " + str(json_status) + " = A successful route call.\n")
-        print("=============================================")
-        print("Directions from " + (orig) + " to " + (dest))
-        print("Trip Duration: " + (json_data["route"]["formattedTime"]))
-        print("Kilometers:: " + str("{:.2f}".format((json_data["route"]["distance"])*1.61)))
-        print("Fuel Used (Ltr): " + str("{:.2f}".format((json_data["route"]["fuelUsed"])*3.78)))
-        print("=============================================")
-        myTable = PrettyTable(["No.","Narrative"])
-        count = 1
-        for each in json_data["route"]["legs"][0]["maneuvers"]:
-            myTable.add_row([count, (each["narrative"]) + " (" + str("{:.2f}".format((each["distance"])*1.61) + " km)")])
-            count+=1
-            #print((each["narrative"]) + " (" + str("{:.2f}".format((each["distance"])*1.61) + " km)"))
-        print(myTable)
-        print("=============================================\n")
-    elif json_status == 402:
-        print("**********************************************")
-        print("Status Code: " + str(json_status) + "; Invalid user inputs for one or both locations.")
-        print("**********************************************\n")
-    elif json_status == 611:
-        print("**********************************************")
-        print("Status Code: " + str(json_status) + "; Missing an entry for one or both locations.")
-        print("**********************************************\n")
+    measurement = input("Choose what type of measurement to use (\'Metric\' or \'Miles\'): ")
+    if measurement == "Metric" or measurement == "metric" :
+        url = main_api + urllib.parse.urlencode({"key":key, "from":orig, "to":dest})
+        json_data = requests.get(url).json()
+        print("URL: " + (url))
+        json_data = requests.get(url).json()
+        json_status = json_data["info"]["statuscode"]
+        if json_status == 0:
+            print("API Status: " + str(json_status) + " = A successful route call.\n")
+            print("=============================================")
+            print("Directions from " + (orig) + " to " + (dest))
+            print("Trip Duration: " + (json_data["route"]["formattedTime"]))
+            print("Kilometers:: " + str("{:.2f}".format((json_data["route"]["distance"])*1.61)))
+            print("Fuel Used (Ltr): " + str("{:.2f}".format((json_data["route"]["fuelUsed"])*3.78)))
+            print("=============================================")
+            myTable = PrettyTable(["No.","Narrative"])
+            count = 1
+            for each in json_data["route"]["legs"][0]["maneuvers"]:
+                myTable.add_row([count, (each["narrative"]) + " (" + str("{:.2f}".format((each["distance"])*1.61) + " km)")])
+                count+=1
+                #print((each["narrative"]) + " (" + str("{:.2f}".format((each["distance"])*1.61) + " km)"))
+            print(myTable)
+            print("=============================================\n")
+        elif json_status == 402:
+            print("**********************************************")
+            print("Status Code: " + str(json_status) + "; Invalid user inputs for one or both locations.")
+            print("**********************************************\n")
+        elif json_status == 611:
+            print("**********************************************")
+            print("Status Code: " + str(json_status) + "; Missing an entry for one or both locations.")
+            print("**********************************************\n")
+        else:
+            print("************************************************************************")
+            print("For Staus Code: " + str(json_status) + "; Refer to:")
+            print("https://developer.mapquest.com/documentation/directions-api/status-codes")
+            print("************************************************************************\n")
+    elif measurement == "Miles" or measurement == "miles" :
+        url = main_api + urllib.parse.urlencode({"key":key, "from":orig, "to":dest})
+        json_data = requests.get(url).json()
+        print("URL: " + (url))
+        json_data = requests.get(url).json()
+        json_status = json_data["info"]["statuscode"]
+        if json_status == 0:
+            print("API Status: " + str(json_status) + " = A successful route call.\n")
+            print("=============================================")
+            print("Directions from " + (orig) + " to " + (dest))
+            print("Trip Duration: " + (json_data["route"]["formattedTime"]))
+            print("Miles: " + str("{:.2f}".format(json_data["route"]["distance"])))
+            print("Fuel Used (Ltr): " + str("{:.2f}".format((json_data["route"]["fuelUsed"])*3.78)))
+            print("=============================================")
+            myTable = PrettyTable(["No.","Narrative"])
+            count = 1
+            for each in json_data["route"]["legs"][0]["maneuvers"]:
+                myTable.add_row([count, (each["narrative"]) + " (" + str("{:.2f}".format(each["distance"]) + " miles)")])
+                count+=1
+                #print((each["narrative"]) + " (" + str("{:.2f}".format((each["distance"])*1.61) + " km)"))
+            print(myTable)
+            print("=============================================\n")
+        elif json_status == 402:
+            print("**********************************************")
+            print("Status Code: " + str(json_status) + "; Invalid user inputs for one or both locations.")
+            print("**********************************************\n")
+        elif json_status == 611:
+            print("**********************************************")
+            print("Status Code: " + str(json_status) + "; Missing an entry for one or both locations.")
+            print("**********************************************\n")
+        else:
+            print("************************************************************************")
+            print("For Staus Code: " + str(json_status) + "; Refer to:")
+            print("https://developer.mapquest.com/documentation/directions-api/status-codes")
+            print("************************************************************************\n")
     else:
-        print("************************************************************************")
-        print("For Staus Code: " + str(json_status) + "; Refer to:")
-        print("https://developer.mapquest.com/documentation/directions-api/status-codes")
-        print("************************************************************************\n")
+        print("Invalid input")
